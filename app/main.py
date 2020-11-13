d1775664fcf200f7e516117de50ba2b82c076b31
Henrique Dias <heukirne@gmail.com>
2020-11-13 13:41:26 -0300

    add alternative

--- app/main.py
+++ app/main.py
@@ -36,6 +36,29 @@ def getName(idPatient):
             'name': 'Paciente ' + str(idPatient)
         }, status.HTTP_400_BAD_REQUEST
 
+@app.route("/patient-raw/<int:idPatient>", methods=['GET'])
+def getRawName(idPatient):
+
+    name = None
+    
+    with cx_Oracle.connect("user", "pass", "127.0.0.1:1521/service") as connection:
+        cursor = connection.cursor()
+        for result in cursor.execute("SELECT nm_paciente FROM schema.paciente WHERE cd_paciente = "+str(idPatient)):
+            name = result[0]
+    
+    if name:
+        return {
+            'status': 'success',
+            'idPatient': idPatient,
+            'name': name
+        }, status.HTTP_200_OK
+    else:
+        return {
+            'status': 'error',
+            'idPatient': idPatient,
+            'name': 'Paciente ' + str(idPatient)
+        }, status.HTTP_400_BAD_REQUEST
+
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=443, ssl_context='adhoc')
