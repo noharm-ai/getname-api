92cd0ebee376419f6de867ad5e5e1624ce9ade1a
Marcelo <marceloarocha@gmail.com>
2023-05-05 09:56:34 -0300

    multiple names endpoint

--- app/main.py
+++ app/main.py
@@ -38,6 +38,39 @@ def getRawName(idPatient):
             'idPatient': idPatient,
             'name': 'Paciente ' + str(idPatient)
         }, status.HTTP_400_BAD_REQUEST
+    
+@app.route("/patient-name/multiple", methods=['POST'])
+def get_multiple():
+    data = request.get_json()
+    list = data.get("patients", [])
+    bind_ids = [":" + str(i + 1) for i in range(len(list))]
+
+    sql = "SELECT DISTINCT(nm_paciente), cd_paciente FROM VW_PACIENTES WHERE cd_paciente IN  (%s)" % (",".join(bind_ids))
+    names = []
+    found = []
+    
+    # TODO: review  mssql
+    #connection = pool.acquire()
+    cursor = connection.cursor()
+    for result in cursor.execute(sql, list):
+        found.append(result[1])
+        names.append({
+            'status': 'success',
+            'idPatient': result[1],
+            'name': result[0]
+        })
+
+    for idPatient in list:
+        if str(idPatient) not in found:
+            names.append({
+                'status': 'error',
+                'idPatient': idPatient,
+                'name': 'Paciente ' + str(idPatient)
+            })
+            
+    #pool.release(connection)
+
+    return names, status.HTTP_200_OK
 
 
 if __name__ == "__main__":
