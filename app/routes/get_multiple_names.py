2adc363fefd5996243bcf9032b0ff1c1c91b706f
Arthur Tabbal <arthurtabbal@gmail.com>
2023-07-07 15:31:19 -0300

    Refactor: use pythonic code convention for file names

---
+++ app/routes/get_multiple_names.py
@@ -0,0 +1,31 @@
+from sqlalchemy import text
+from flask import request
+from flask_api import status
+from connections import engine, MULTI_QUERY
+
+
+def get_multiple_names():
+    data = request.get_json()
+    ids_list = data.get("patients", [])
+
+    sql = MULTI_QUERY.format(",".join([str(i) for i in ids_list]))
+    names = []
+    found = []
+
+    with engine.connect() as connection:
+        result = connection.execute(text(sql))
+        for row in result:
+            found.append(row.id)
+            names.append({"status": "success", "idPatient": row.id, "name": row.name})
+
+    for id_patient in ids_list:
+        if id_patient not in found:
+            names.append(
+                {
+                    "status": "error",
+                    "idPatient": id_patient,
+                    "name": "Paciente " + str(id_patient),
+                }
+            )
+
+    return names, status.HTTP_200_OK
---
+++ app/routes/get_name.py
@@ -0,0 +1,24 @@
+from sqlalchemy import text
+from flask_api import status
+from connections import engine, QUERY
+
+
+def get_name(idPatient):
+    name = None
+    with engine.connect() as connection:
+        result = connection.execute(text(QUERY.format(idPatient)))
+        for row in result:
+            name = row.name
+
+    if name:
+        return {
+            "status": "success",
+            "idPatient": idPatient,
+            "name": name,
+        }, status.HTTP_200_OK
+    else:
+        return {
+            "status": "error",
+            "idPatient": idPatient,
+            "name": "Paciente " + str(idPatient),
+        }, status.HTTP_400_BAD_REQUEST
