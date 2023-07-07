c77380fa1f2fc4978d2d8be9791ab9301caa3076
Arthur Tabbal <arthurtabbal@gmail.com>
2023-07-07 15:30:25 -0300

    Refactor: use pythonic code conventions

--- app/main.py
+++ app/main.py
@@ -1,6 +1,6 @@
 from routes.hello import hello
-from routes.getName import getName
-from routes.getMultipleNames import getMultipleNames
+from routes.get_name import get_name
+from routes.get_multiple_names import get_multiple_names
 from flask_api import FlaskAPI
 from flask_cors import CORS
 from connections import FLASK_PORT
@@ -10,8 +10,10 @@ app = FlaskAPI(__name__)
 CORS(app)
 
 app.add_url_rule("/", view_func=hello)
-app.add_url_rule("/patient-name/<int:idPatient>", view_func=getName, methods=["GET"])
-app.add_url_rule("/patient-name/multiple", view_func=getMultipleNames, methods=["POST"])
+app.add_url_rule("/patient-name/<int:idPatient>", view_func=get_name, methods=["GET"])
+app.add_url_rule(
+    "/patient-name/multiple", view_func=get_multiple_names, methods=["POST"]
+)
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=FLASK_PORT, ssl_context="adhoc")
