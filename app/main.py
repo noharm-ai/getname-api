0d8e23b617f6e0c9e2f02f55c56890f5d219721b
Arthur Tabbal <arthurtabbal@gmail.com>
2023-08-04 16:31:06 -0300

    Add route to clear cache

--- app/main.py
+++ app/main.py
@@ -1,6 +1,7 @@
 from routes.hello import hello
 from routes.get_name import get_name
 from routes.get_multiple_names import get_multiple_names
+from routes.clear_cache import clear_cache
 from flask_api import FlaskAPI
 from flask_cors import CORS
 from resources.cache import cache, config
@@ -10,11 +11,15 @@ CORS(app)
 
 cache.init_app(app, config)
 
+# Rotas de views
 app.add_url_rule("/", view_func=hello)
 app.add_url_rule("/patient-name/<int:idPatient>", view_func=get_name, methods=["GET"])
 app.add_url_rule(
     "/patient-name/multiple", view_func=get_multiple_names, methods=["POST"]
 )
+# Rota de limpeza do dache
+app.add_url_rule("/patient-name/clear", view_func=clear_cache, methods=["GET"])
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", ssl_context="adhoc")
--- app/resources/cache.py
+++ app/resources/cache.py
@@ -4,6 +4,7 @@ config = {
     "CACHE_TYPE": "FileSystemCache",
     "CACHE_DEFAULT_TIMEOUT": 3600,
     "CACHE_DIR": "resources/.cache",
+    "CACHE_THRESHOLD": 1000,
 }
 
 
