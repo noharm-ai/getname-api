b32025508b9f63e1471bcc7ac23215c59fce7dfe
Arthur Tabbal <arthurtabbal@gmail.com>
2023-08-23 13:52:26 -0300

    Load cache params from .env

--- app/resources/cache.py
+++ app/resources/cache.py
@@ -1,11 +1,16 @@
 from flask_caching import Cache
+from dotenv import load_dotenv
+import os
+
+HOUR = 3600
+
+load_dotenv()
 
 config = {
     "CACHE_TYPE": "FileSystemCache",
-    "CACHE_DEFAULT_TIMEOUT": 3600,
+    "CACHE_DEFAULT_TIMEOUT": os.getenv("CACHE_TIMEOUT", 1) * HOUR,
     "CACHE_DIR": "resources/.cache",
-    "CACHE_THRESHOLD": 1000,
+    "CACHE_THRESHOLD": os.getenv("CACHE_THRESHOLD", 1000),
 }
-
 
 cache = Cache()
