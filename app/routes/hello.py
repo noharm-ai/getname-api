b0a433df7eed821a593384f3a65274a4d4d47b00
Arthur Tabbal <arthurtabbal@gmail.com>
2023-08-23 14:01:59 -0300

    Update hello message

--- app/routes/hello.py
+++ app/routes/hello.py
@@ -1,2 +1,2 @@
 def hello():
-    return "Serviço de nomes habilitado! Volte para a NoHarm e use o sistema normalmente ;)"
+    return "NoHarm - GetName 1.1\n\nServiço de nomes habilitado! Volte para a NoHarm e use o sistema normalmente ;)\n\n"
