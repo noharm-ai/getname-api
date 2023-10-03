df953e4f138e8d17fc7ac3f71f2c82e1d8d2765b
Arthur Tabbal <arthurtabbal@gmail.com>
2023-10-03 11:21:57 -0300

    Bugfix: convert cache params to ints

--- app/resources/cache.py
+++ app/resources/cache.py
@@ -8,9 +8,9 @@ load_dotenv()
 
 config = {
     "CACHE_TYPE": "FileSystemCache",
-    "CACHE_DEFAULT_TIMEOUT": os.getenv("CACHE_TIMEOUT", 1) * HOUR,
+    "CACHE_DEFAULT_TIMEOUT": int(os.getenv("CACHE_TIMEOUT", 1)) * HOUR,
     "CACHE_DIR": "resources/.cache",
-    "CACHE_THRESHOLD": os.getenv("CACHE_THRESHOLD", 1000),
+    "CACHE_THRESHOLD": int(os.getenv("CACHE_THRESHOLD", 1000)),
 }
 
 cache = Cache()
