12142464499cb65da8e2f7a0daadcca8a75c7b14
Henrique Dias <heukirne@gmail.com>
2024-11-28 18:19:45 -0300

    Update get_name.py

--- app/routes/get_name.py
+++ app/routes/get_name.py
@@ -15,16 +15,19 @@ def get_name(idPatient):
         )
         for row in result:
             name = row[0]
+            data = {key: value for key, value in row._mapping.items()}
 
     if name:
         return {
             "status": "success",
             "idPatient": idPatient,
             "name": name,
+            "data": data,
         }, status.HTTP_200_OK
     else:
         return {
             "status": "error",
             "idPatient": idPatient,
             "name": "Paciente " + str(idPatient),
+            "data": {},
         }, status.HTTP_400_BAD_REQUEST
