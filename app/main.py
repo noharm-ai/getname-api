d124419e82dfab52ebbf2ff6533838a8a22266e7
Henrique Dias <heukirne@gmail.com>
2024-12-04 17:54:04 -0300

    Update main.py

--- app/main.py
+++ app/main.py
@@ -2,6 +2,7 @@ from routes.hello import hello
 from routes.get_name import get_name
 from routes.get_multiple_names import get_multiple_names
 from routes.clear_cache import clear_cache
+from routes.get_search_name import search_name
 from flask_api import FlaskAPI
 from flask_cors import CORS
 from resources.cache import cache, config
@@ -14,6 +15,7 @@ cache.init_app(app, config)
 # Rotas de views
 app.add_url_rule("/", view_func=hello)
 app.add_url_rule("/patient-name/<int:idPatient>", view_func=get_name, methods=["GET"])
+app.add_url_rule("/search-name/<string:partial_name>", view_func=search_name, methods=["GET"])
 app.add_url_rule(
     "/patient-name/multiple", view_func=get_multiple_names, methods=["POST"]
 )
